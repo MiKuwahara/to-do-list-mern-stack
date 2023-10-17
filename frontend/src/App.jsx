5970c2602cd746bb8ad6fb26d29844f6a84d228d
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-10-16 23:29:17 -0500

    Add CreateToDo page to create todo in React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import DeleteToDo from "./pages/DeleteToDo.jsx";
+import CreateToDo from './pages/CreateToDo.jsx';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/todolist/delete/:id" element={<DeleteToDo />}></Route>
+      <Route path="/todolist/create" element={<CreateToDo />} ></Route>
     </Routes>
   )
 }
